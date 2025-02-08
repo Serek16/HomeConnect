1d242b935a6c7ad9fddba33688a46588a1fd39ef
Sergiusz <xbxbcvbxc@gmail.com>
2025-02-08 13:24:46 +0100

    Connect ESP32 to online MQTT test broker

--- sketch_feb8a.ino
+++ sketch_feb8a.ino
@@ -1,5 +1,14 @@
 #include <WiFi.h>
-#include <WiFiCredentials.h>
+#include <MQTTClient.h>
+#include <ArduinoJson.h>
+#include "MyCredentials.h"
+
+const int PUBLISH_INTERVAL = 5000; // 5 seconds
+
+WiFiClient network;
+MQTTClient mqtt = MQTTClient(256);
+
+unsigned long lastPublishTime = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -12,8 +21,68 @@ void setup() {
     printf(".");
   }
   printf("\nSuccessfully connected\n");
+
+  connectToMQTT();
 }
 
 void loop() {
+  mqtt.loop();
 
+  if (millis() - lastPublishTime > PUBLISH_INTERVAL) {
+    sendToMQTT();
+    lastPublishTime = millis();
+  }
+}
+
+void connectToMQTT() {
+  // Connect to the MQTT broker
+  mqtt.begin(MQTT_BROKER_ADRRESS, MQTT_PORT, network);
+
+  // Create a handler for incoming messages
+  mqtt.onMessage(messageHandler);
+
+  Serial.print("ESP32 - Connecting to MQTT broker");
+
+  while (!mqtt.connect(MQTT_CLIENT_ID, MQTT_USERNAME, MQTT_PASSWORD)) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+
+  if (!mqtt.connected()) {
+    Serial.println("ESP32 - MQTT broker Timeout!");
+    return;
+  }
+
+  // Subscribe to a topic, the incoming messages are processed by messageHandler() function
+  if (mqtt.subscribe(SUBSCRIBE_TOPIC))
+    Serial.print("ESP32 - Subscribed to the topic: ");
+  else
+    Serial.print("ESP32 - Failed to subscribe to the topic: ");
+
+  Serial.println(SUBSCRIBE_TOPIC);
+  Serial.println("ESP32 - MQTT broker Connected!");
+}
+
+void sendToMQTT() {
+  StaticJsonDocument<200> message;
+  message["timestamp"] = millis();
+  message["data"] = analogRead(0);  // Or you can read data from other sensors
+  char messageBuffer[512];
+  serializeJson(message, messageBuffer);
+
+  mqtt.publish(PUBLISH_TOPIC, messageBuffer);
+
+  Serial.println("ESP32 - sent to MQTT:");
+  Serial.print("- topic: ");
+  Serial.println(PUBLISH_TOPIC);
+  Serial.print("- payload:");
+  Serial.println(messageBuffer);
+}
+
+void messageHandler(String &topic, String &payload) {
+  Serial.println("ESP32 - received from MQTT:");
+  Serial.println("- topic: " + topic);
+  Serial.println("- payload:");
+  Serial.println(payload);
 }
