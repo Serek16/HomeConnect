f3e87653f344e861502f9f6e00dab7f2e60ee008
Sergiusz <xbxbcvbxc@gmail.com>
2026-02-23 18:24:17 +0100

    Setup OTA

--- sketch_feb8a.ino
+++ sketch_feb8a.ino
@@ -4,6 +4,7 @@
 #include "PCPower.h"
 
 #include <USBHIDKeyboard.h>
+#include <ArduinoOTA.h>
 
 const char* MQTT_COMMAND_TOPIC       = "ssh-home-cmd";
 const char* MQTT_RESPONSE_TOPIC      = "ssh-home-res";
@@ -39,6 +40,8 @@ void setup() {
   mqtt.subscribe(MQTT_COMMAND_TOPIC);
 
   Keyboard.begin();
+
+  setupOTA();
 
   pinMode(POWER_SW_PIN, OUTPUT);
   pinMode(POWER_LED_PIN, INPUT);
@@ -176,4 +179,58 @@ void bootOS(int bootNumber) {
     Keyboard.releaseAll();
     delay(50);
   }
+}
+
+void setupOTA() {
+  // Hostname for OTA (will appear as "ESP32-PC-Controller" on network)
+  ArduinoOTA.setHostname("ESP32-S3-SSH-HOME");
+
+  // Password for OTA (OPTIONAL but RECOMMENDED for security)
+  ArduinoOTA.setPassword("X^7:F8\"GjN,n^PA");  // Uncomment and set password
+
+  // Port defaults to 3232
+  // ArduinoOTA.setPort(3232);
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {  // U_SPIFFS
+      type = "filesystem";
+    }
+    Serial.println("Start updating " + type);
+
+    // Turn off keyboard and other peripherals during update
+    digitalWrite(LED_BUILTIN, HIGH);
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+    digitalWrite(LED_BUILTIN, LOW);
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    // Blink LED during update
+    digitalWrite(LED_BUILTIN, (millis() / 250) % 2);
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+    digitalWrite(LED_BUILTIN, LOW);
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("OTA Ready");
 }
